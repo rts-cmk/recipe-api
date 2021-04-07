b04d271b381accdb66e56ceac7371c19993f34ff
Brian Emilius <be@rts.dk>
2021-04-07 11:24:50 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1869368e3a506411296ea8d3b3ef410f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9c2e1f88-0d65-4e84-a3a9-df4baefabd2e'
+};
